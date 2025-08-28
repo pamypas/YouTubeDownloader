5b099e9bcb58761ef50548507fc4b05f3d3910ca
Anatoly Belyaev <jikarto@gmail.com>
2025-08-28 19:43:59 +0300

    feat: add README and update test script to use youtube_downloader.py

--- app/test.sh
+++ app/test.sh
@@ -3,4 +3,4 @@ TEST_MSG='{"url":"https://youtube.com/watch?v=dQw4w9WgXcQ", "action": "audio"}'
 LEN=$(printf "%08x" ${#TEST_MSG})
 
 # Отправляем в скрипт (длина + сообщение)
-echo -n -e "\\x${LEN:0:2}\\x${LEN:2:2}\\x${LEN:4:2}\\x${LEN:6:2}${TEST_MSG}" | python3 test.py
+echo -n -e "\\x${LEN:0:2}\\x${LEN:2:2}\\x${LEN:4:2}\\x${LEN:6:2}${TEST_MSG}" | python3 youtube_downloader.py
